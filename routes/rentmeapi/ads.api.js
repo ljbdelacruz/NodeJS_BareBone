bdb51cee1d06c8ca9b615d7677b38f26a64f8fd5
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-07-08 17:08:16 +0800

    enhance the api

--- routes/rentmeapi/ads.api.js
+++ routes/rentmeapi/ads.api.js
@@ -5,31 +5,30 @@
 var express = require('express');
 var router = express.Router();
 const dummy=require('../../app/seeders/dummy.data')
+const global=require('../../app/config/global')
 
 router.get('/getAds', function(req, res, next){
-    res.send(dummy.ads);
+    if(global.demo){
+      res.send(dummy.ads);
+    }else{
+      //implement repo fetching here
+      res.send()
+    }
 });
 
 router.get('/getRecommendedAds', function(req, res, next){
-  res.send([
-   {images:[], title:'Jojos Crib', desc:'3 Bed Room All', price:'$40', priceSubtitle:'Per Night', 
-    owner:{title:'Detective Pikachu', subtitle:'Owner', profileImage:'', rightImage:''},
-    review:{review:3, reviewer:120}
-   },
-    {images:[], title:'Club House', desc:'3 Bed Room All around for you this is the best place for people who are planning on starting a family', price:'$40', priceSubtitle:'Per Night', owner:{title:'Squirtle Mash', subtitle:'Co-Host', profileImage:'', rightImage:''},
-    review:{review:3.5, reviewer:11}
-   },
-   {images:[], title:'Sports Club ', desc:'Perfect for hosting your birthday parties and social events Perfect for hosting your birthday parties and social events Perfect for hosting your birthday parties and social events Perfect for hosting your birthday parties and social events Perfect for hosting your birthday parties and social events', price:'$400', priceSubtitle:'Per Night', owner:{title:'Squirtle Mash', subtitle:'Co-Host', profileImage:'', rightImage:''},
-    review:{review:4, reviewer:89}
-   }
-  ])
-  
+  if(global.demo){
+    res.send(dummy.rads)
+  }else{
+    res.send();
+  }  
 })
 
 router.get('/getAdsByUser', function(req, res, next){
-    res.send([
-      
-    ])
+    if(global.demo){
+      res.send(dummy.ads)
+    }
+
 })
 
 router.get('/getByUserCategory', function(req, res, next){
